def92cd2a8cb717def8ea1fd6b89ce6cd84b7f69
Albert Mendoza <albertmendoza0462@gmail.com>
2022-09-28 19:22:59 -0400

    Se editó el archivo "Screen"

--- app/src/main/java/edu/ucne/parcial1_albert/utils/Screen.kt
+++ app/src/main/java/edu/ucne/parcial1_albert/utils/Screen.kt
@@ -1,6 +1,6 @@
 package edu.ucne.parcial1_albert.utils
 
-sealed class Screen(Route: String) {
+sealed class Screen(val Route: String) {
     object RegistroArticuloScreen: Screen("RegistroArticuloScreen")
     object ConsultaArticuloScreen: Screen("ConsultaArticulosScreen")
 }
