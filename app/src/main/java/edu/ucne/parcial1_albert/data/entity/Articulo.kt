8fc0acf576852033b249afb07930292a2b0114cb
Albert Mendoza <albertmendoza0462@gmail.com>
2022-09-28 18:15:34 -0400

    Se agregó el modelo "Articulo"

---
+++ app/src/main/java/edu/ucne/parcial1_albert/data/entity/Articulo.kt
@@ -0,0 +1,13 @@
+package edu.ucne.parcial1_albert.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Articulos")
+data class Articulo(
+    @PrimaryKey(autoGenerate = true)
+    val ArticuloId: Int=0,
+    val Descripcion: String="",
+    val Marca: String="",
+    val Existencia: Int=0
+)
