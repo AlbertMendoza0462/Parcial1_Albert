ae75d17418e47eb68637a90d40cf2c08e1c460c0
Albert Mendoza <albertmendoza0462@gmail.com>
2022-09-28 19:18:45 -0400

    Se editó el archivo "ArticuloScreen" y se dejó sin validaciones

--- app/src/main/java/edu/ucne/parcial1_albert/ui/entity/ArticuloScreen.kt
+++ app/src/main/java/edu/ucne/parcial1_albert/ui/entity/ArticuloScreen.kt
@@ -2,12 +2,11 @@ package edu.ucne.parcial1_albert.ui.entity
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -29,12 +28,34 @@ fun ArticuloScreen(
         ) {
             Column(
                 modifier = Modifier
+                    .fillMaxWidth()
                     .padding(it)
                     .padding(8.dp)
             ) {
-                OutlinedButton(onClick = {
-                    navHostController.navigateUp()
-                }) {
+                OutlinedTextField(
+                    label = { Text(text = "Descripcion") },
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    value = viewModel.Descripcion,
+                    onValueChange = { viewModel.Descripcion = it })
+                OutlinedTextField(
+                    label = { Text(text = "Marca") },
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    value = viewModel.Marca,
+                    onValueChange = { viewModel.Marca = it })
+                OutlinedTextField(
+                    label = { Text(text = "Existencia") },
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    value = viewModel.Existencia.toString(),
+                    onValueChange = {
+                        viewModel.Existencia = it.toInt()
+                    })
+                OutlinedButton(
+                    onClick = {
+                        navHostController.navigateUp()
+                        viewModel.Guardar()
+                    },
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                ) {
                     Text(text = "Guardar")
                 }
             }
